5cd4cea27c57fb9c582273ac81b61cb89e0f6b84
StvL <fyd789@sohu.com>
2025-08-06 21:59:15 +0800

    new try for sessionID

--- proxy.js
+++ proxy.js
@@ -1,9 +1,13 @@
 
+const uuid = require('uuid');
 const WebSocket = require('ws');
 const ReconnectingWebSocket = require('reconnecting-websocket');
 
+// 存储所有客户端连接
+const sessionIds = new Map();
+
 const targetConnection = new ReconnectingWebSocket('ws://127.0.0.1:16888/ks/printer', [], {
-  WebSocket: WebSocket,          // add due to pack exe problem pkg not support node22
+  WebSocket: WebSocket,          // add due to build package problem, pkg not support node22
   // 重连配置
   reconnectInterval: 1000,       // 初始重连间隔(ms)
   maxReconnectInterval: 60000,   // 最大重连间隔
@@ -18,25 +22,37 @@ let sourceConnection = null;
 const proxyServer = new WebSocket.Server({ port: 6888 });
 console.log('proxyServer running at ws://localhost:6888');
 
+// proxy connection
+proxyServer.on('connection', (client) => {   
+  const sessionId = uuid.v4();
+  sessionIds.set(sessionId, client);
+
+  console.log(client._socket.remotePort);
+
+  console.log(`New clinet connected : ${sessionId}`);
+  sourceConnection = client;
+
+  // 客户端消息转发到目标服务器
+  client.on('message', (data) => {
+    console.log(`Forward msg to kuaishou: ${data}`);
+    targetConnection.send(data.toString('utf8'));      
+  });
+});
+
+// proxy clinet close
+proxyServer.on('close',(client) => {
+  console.log(`clinet disconnected : ${sessionId}`);
+  sessionIds.delete(sessionId);
+});
+
 targetConnection.addEventListener('open', () => {
   console.log('Connected to local kuaishou print tool');
-
-  // proxy connection
-  proxyServer.on('connection', (client) => {   
-    console.log(`New clinet connected`);    
-    sourceConnection = client;
-
-    // 客户端消息转发到目标服务器
-    client.on('message', (data) => {
-      console.log(`Forward msg to kuaishou: ${data}`);
-      targetConnection.send(data.toString('utf8'));      
-    });
-  });
-
 });
 
 targetConnection.addEventListener('message', (event) => {
   console.log('Received:', event.data);
+  /// check by ID 
+
   sourceConnection.send(event.data);
 });
 
