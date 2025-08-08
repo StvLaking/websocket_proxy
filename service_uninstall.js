a96fde84e4544ad24d39fb616126af949ce314c3
StvL <fyd789@sohu.com>
2025-08-08 15:33:58 +0800

    remove special path

--- service_uninstall.js
+++ service_uninstall.js
@@ -4,7 +4,6 @@ const { Service } = require('node-windows');
 const svc = new Service({
   name: 'KSProxyService',
   description : 'Kuaishou proxy Service',
-  //script : "C:\\Users\\stvL\\Documents\\websocket_proxy\\proxy.js",
   script: require('path').join(__dirname, 'proxy.js'),
   logrotate: {
     maxSize: '1m',    // 单个日志文件最大1MB
