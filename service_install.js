e4ace4521d429b7433236ec0d108740e866debd6
Steven Laking <fyd789@sohu.com>
2025-08-08 14:57:29 +0800

    Update service_install.js
    add log size for install

--- service_install.js
+++ service_install.js
@@ -4,7 +4,13 @@ const { Service } = require('node-windows');
 const svc = new Service({
   name: 'KSProxyService',
   description : 'Kuaishou proxy Service',
-  script : "C:\\Users\\stvL\\Documents\\websocket_proxy\\proxy.js"
+  // script : "C:\\Users\\stvL\\Documents\\websocket_proxy\\proxy.js"
+  script : require('path').join(__dirname, 'proxy.js'),
+  logrotate: {
+    maxSize: '1m',    // 单个日志文件最大1MB
+    keep: 3,          // 保留3个备份
+    compress: true    // 压缩旧日志
+  }
 });
 
 svc.on('install', () => {
